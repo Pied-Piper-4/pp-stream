aa2e360eedc5f8b0d248cdef2198029d3ca9460e
Boanerges1996 <samsonnkrumah253@gmail.com>
2022-09-04 03:24:12 +0530

    Added default thumbnail for meetings created

--- apps/backends/user-service/src/apps/meeting/meeting.model.ts
+++ apps/backends/user-service/src/apps/meeting/meeting.model.ts
@@ -55,6 +55,16 @@ export class Meeting {
     type: String,
   })
   link: string;
+
+  @Prop({
+    default: [
+      'https://firebasestorage.googleapis.com/v0/b/pp-stream.appspot.com/o/Bg1.png?alt=media&token=2c60e303-16c1-4c65-8221-0f97b687ead3',
+      'https://firebasestorage.googleapis.com/v0/b/pp-stream.appspot.com/o/Bg2.png?alt=media&token=579d27bd-1a6f-4123-b2cd-181b3a2200e2',
+      'https://firebasestorage.googleapis.com/v0/b/pp-stream.appspot.com/o/Bg3.png?alt=media&token=f83d324f-294a-4b8b-8cc5-1e54e32d45bf',
+      'https://firebasestorage.googleapis.com/v0/b/pp-stream.appspot.com/o/Bg4.png?alt=media&token=5790a00d-8ec4-49cf-a695-e1e1df6936e5',
+    ].at(Math.random() * 4),
+  })
+  thumbnail: string;
 }
 
 export const MeetingSchema = SchemaFactory.createForClass(Meeting);
