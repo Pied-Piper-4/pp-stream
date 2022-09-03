b564f3ac570dd5c78630232b3368e709f073235c
Boanerges1996 <samsonnkrumah253@gmail.com>
2022-09-03 15:39:42 +0530

    Added swagger docs

--- apps/backends/user-service/src/main.ts
+++ apps/backends/user-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './apps/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
@@ -9,6 +10,14 @@ async function bootstrap() {
   const port = configService.get('port');
   const globalPrefix = configService.get('globalPrefix');
 
+  const config = new DocumentBuilder()
+    .setTitle('PP Stream User Service')
+    .setDescription('The Piper Stream User Service API description')
+    .setVersion('1.0')
+    .addTag('pp-user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('user-service/api', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
