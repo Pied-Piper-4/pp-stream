47ff2c6b9a61b3fa5afc56b7f0bf1b5495d347e0
Anshul Sharma <cehanshul@gmail.com>
2022-09-03 21:49:08 +0530

    y

--- apps/frontend/pp-stream/src/components/Footer/Footer.tsx
+++ apps/frontend/pp-stream/src/components/Footer/Footer.tsx
@@ -4,23 +4,7 @@ export const Footer = () => {
   return (
     <div>
       <hr />
-      <div className="flex justify-between ">
-        <div className="flex self-end">
-          <img className="h-12" src="/images/logo.jpg" alt="" />
-          <p>iedPiper</p>
-        </div>
-        <div className="flex space-x-2">
-          <p>Appstore</p>
-          <p>Playstore</p>
-        </div>
-      </div>
-      <div className="flex justify-between ">
-        <p>Copyright © ppstream 2022, Inc all rights received</p>
-        <div className="flex space-x-2">
-          <p>Appstore</p>
-          <p>Playstore</p>
-        </div>
-      </div>
+   
     </div>
   );
 };
