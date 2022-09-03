e757f886edd0f4784fa209b0875e091bbb1e43b3
Boanerges1996 <samsonnkrumah253@gmail.com>
2022-09-03 17:43:21 +0530

    docker-compose path added

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ ssh root@143.110.185.106<<EOF
     pwd
     cd /root/pp-stream
     git pull
+    cd /root/pp-stream/apps/backends
     docker-compose down
     docker-compose up -d --build
     exit
